b0b9c9d0dd6f50286aadce32bfa58d12a06c2d2a
MD. AL-AMIN <amin57.cse@gmail.com>
2026-04-09 22:46:29 +0600

    add stories page

---
+++ hello-next/src/app/stories/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Title from '../components/Title'
+
+const Stories = () => {
+  return (
+    <div>
+      <Title> Explore Stories of developers</Title>
+    </div>
+  )
+}
+
+export default Stories
