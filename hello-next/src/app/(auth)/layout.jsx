b1452fd82026cb77c78511cb836a9022e7d2d669
MD. AL-AMIN <amin57.cse@gmail.com>
2026-04-16 23:25:52 +0600

    add auth layout

---
+++ hello-next/src/app/(auth)/layout.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const AuthLayout = ({children}) => {
+  return (
+    <div className='flex min-h-screen gap-5'>
+      <div className='flex-2'>
+        {children}
+      </div>
+      <div className='flex-1 bg-amber-300 flex justify-center items-center'>
+        <h1 className='text-4xl font-bold'>
+            Welcome to next js
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default AuthLayout
